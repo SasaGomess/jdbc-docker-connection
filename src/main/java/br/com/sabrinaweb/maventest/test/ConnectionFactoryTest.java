464c5ac1ff9a2fb9844fd8d409e3ad13ab635bb8
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-06-24 20:15:34 -0300

    feat: create the connection with connectionFactory

---
+++ src/main/java/br/com/sabrinaweb/maventest/test/ConnectionFactoryTest.java
@@ -0,0 +1,9 @@
+package br.com.sabrinaweb.maventest.test;
+
+import br.com.sabrinaweb.maventest.ConnectionFactory;
+
+public class ConnectionFactoryTest {
+    public static void main(String[] args) {
+        ConnectionFactory.getConnection();
+    }
+}
