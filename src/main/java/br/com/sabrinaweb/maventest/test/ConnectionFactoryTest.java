a79e32af1cd6ee8cbcaebae72ed11bd84d238db2
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-06-24 20:20:57 -0300

    update: the connectionFactory for not return null

--- src/main/java/br/com/sabrinaweb/maventest/test/ConnectionFactoryTest.java
+++ src/main/java/br/com/sabrinaweb/maventest/test/ConnectionFactoryTest.java
@@ -1,6 +1,6 @@
 package br.com.sabrinaweb.maventest.test;
 
-import br.com.sabrinaweb.maventest.ConnectionFactory;
+import br.com.sabrinaweb.maventest.repository.ConnectionFactory;
 
 public class ConnectionFactoryTest {
     public static void main(String[] args) {
