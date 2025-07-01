ea47642771c8e92157f9a2bafc6c70023d41afb4
SasaGomes <sabrina.gomess.dev@gmail.com>
2025-07-01 08:32:00 -0300

    update: fixing the menu to work with producer and anime operations

--- src/main/java/br/com/sabrinaweb/maventest/projectJdbc/view/Program.java
+++ src/main/java/br/com/sabrinaweb/maventest/projectJdbc/view/Program.java
@@ -1,5 +1,6 @@
 package br.com.sabrinaweb.maventest.projectJdbc.view;
 
+import br.com.sabrinaweb.maventest.projectJdbc.service.AnimeService;
 import br.com.sabrinaweb.maventest.projectJdbc.service.ProducerService;
 
 import java.util.Scanner;
@@ -10,19 +11,48 @@ public class Program {
     public static void main(String[] args) {
         int op = 0;
         do {
-            producerMenu();
+            menu();
             op = Integer.parseInt(SCANNER.nextLine());
-            ProducerService.menu(op);
+            switch (op) {
+                case 1 -> {
+                    producerMenu();
+                    op = Integer.parseInt(SCANNER.nextLine());
+                    ProducerService.menu(op);
+                }
+                case 2 -> {
+                    animeMenu();
+                    op = Integer.parseInt(SCANNER.nextLine());
+                    AnimeService.menu(op);
+                }
+            }
+
         } while (op != 0);
     }
 
     private static void producerMenu() {
         System.out.println("Type the number of operation");
-        System.out.println("1. Search for producer (findAll or findByName)");
-        System.out.println("2. Delete producer");
-        System.out.println("3. Save producer");
-        System.out.println("4. Update producer");
-        System.out.println("0. To Exit");
+        System.out.println("1. Search for anime (findAll or findByName)");
+        System.out.println("2. Delete anime");
+        System.out.println("3. Save anime");
+        System.out.println("4. Update anime");
+        System.out.println("9. Go Back");
         System.out.print("RESP: ");
     }
+
+    private static void menu() {
+        System.out.println("1. Producer");
+        System.out.println("2. Anime");
+        System.out.println("0. Exit");
+    }
+
+    private static void animeMenu() {
+        System.out.println("Type the number of operation");
+        System.out.println("1. Search for anime (findAll or findByName)");
+        System.out.println("2. Delete anime");
+        System.out.println("3. Save anime");
+        System.out.println("4. Update anime");
+        System.out.println("9. Go Back");
+        System.out.print("RESP: ");
+    }
+
 }
